f4f69b1ede5c314d1c866ad7655ef65e7a6f31f1
Mohammad Hosein Ronaghi <mhrownaghi@gmail.com>
2019-10-05 17:29:26 +0330

    Fast Doubling Fibonacci Algorithm

---
+++ fibonacci.py
@@ -0,0 +1,28 @@
+
+"""This is Fast Doubling Algorithms for fibonacci.
+
+Author: mhrownaghi
+Github Page: https://github.com/mhrownaghi
+
+"""
+
+
+def fibonacci(n):
+    """(Public) Returns F(n)."""
+    if n < 0:
+        raise ValueError("Negative arguments not implemented")
+    return _fib(n)[0]
+
+
+# (Private) Returns the tuple (F(n), F(n+1)).
+def _fib(n):
+    if n == 0:
+        return (0, 1)
+    else:
+        a, b = _fib(n // 2)
+        c = a * (b * 2 - a)
+        d = a * a + b * b
+        if n % 2 == 0:
+            return (c, d)
+        else:
+            return (d, c + d)
